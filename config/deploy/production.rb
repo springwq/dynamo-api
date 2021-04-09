cd357e604c202260037bf3b6511e008d6a6dab06
steven <wuqi0509@gmail.com>
2021-04-09 16:52:20 +0800

    Fix typo

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '3.238.24.77 ', user: 'steven', roles: %w{app db web}
+server '3.238.24.77', user: 'steven', roles: %w{app db web}
 
 # role-based syntax
 # ==================
